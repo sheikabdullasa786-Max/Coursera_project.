de6da9b469feda47a374dfcae222db4c4a32a49c
sheikabdullasa786-Max <sheikabdullasa786@gmail.com>
2025-12-25 13:16:24 +0530

    Create App component with initial structure

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import AboutUs from "./AboutUs";
+import ProductList from "./ProductList";
+import CartItem from "./CartItem";
+
+function App() {
+  return (
+    <div>
+      <h1>Paradise Nursery</h1>
+      <button>Get Started</button>
+
+      <hr />
+
+      <AboutUs />
+      <ProductList />
+      <CartItem />
+    </div>
+  );
+}
+
+export default App;
